6636e41226e08849f4329150524cd679bc5302a6
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2021-02-09 14:19:34 -0600

    Update OverloadingMethodExample.cs

--- OverloadingMethodExample.cs
+++ OverloadingMethodExample.cs
@@ -1,6 +1,12 @@
 using System;
 class HelloWorld
 {
+      //When you overload a method. The methods you create can all have the same name, but what matters is the parameter list.
+      //If two or more of the methods have the same parameter list. Then there will be an error.
+      //Each method can have the same identifier, but at least ONE DIFFERENCE in the parameter list.
+      //The program will automatically know which method to use based on the parameters/arguments you input in the called method.
+      
+      
       static void Main() 
       {
         Display("Sam", "Green");
