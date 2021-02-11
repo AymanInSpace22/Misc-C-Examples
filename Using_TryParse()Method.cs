e64739de0970ec20977404d2042c0f24af0d6c64
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2021-02-10 20:33:20 -0600

    Update Using_TryParse()Method.cs

--- Using_TryParse()Method.cs
+++ Using_TryParse()Method.cs
@@ -27,3 +27,28 @@ namespace ConsoleApp1
         }
     }
 }
+
+
+
+
+//Another example of something you can do with TryParse()
+
+using System;
+class HelloWorld 
+{
+    
+    //The TryParse() can return any value you want. Not just 0
+    //To do this you must put the if() around it with the Not(!) character in front
+    
+  static void Main()
+  {
+    string entryString;
+    int score;
+    Console.Write("Enter your test score >> ");
+    entryString = Console.ReadLine();
+    if(!int.TryParse(entryString, out score));
+        score = -1;
+    Console.WriteLine("You entered {0}", score);
+  }
+}
+
