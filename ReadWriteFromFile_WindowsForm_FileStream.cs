06f91956a9355e5697c085fb7708b0e03c8ed050
Ayman <70823613+AymanInSpace22@users.noreply.github.com>
2021-04-20 14:21:10 -0500

    Update ReadWriteFromFile_WindowsForm_FileStream.cs

--- ReadWriteFromFile_WindowsForm_FileStream.cs
+++ ReadWriteFromFile_WindowsForm_FileStream.cs
@@ -20,25 +20,24 @@ namespace WindowsFormsAppStreamReaderStreamWriter
         private void saveButton_Click(object sender, EventArgs e)
         {
             // anytime you write to a file - you use create
-            // also this send it to the default folder
-            // you can define your own path
-            // open it
+
+         // doing it this way will save the file to the project you are currently in
             //FileStream fs = new FileStream("employee.txt", FileMode.Create, FileAccess.Write);
-            // putting the '@' symbol allows you to use only one slash instead of two
-            // typically they wont let you 
+
+         // putting the '@' symbol allows you to use only one slash instead of two. Usually they wont let you put one slash so you must prefix the line with the '@' symbol
+            // FileStream fs = new FileStream(@"C:\\users\boula\desktop\employee.txt", FileMode.Create, FileAccess.Write);
 
             saveFileDialog1.ShowDialog();
             //string path = pathTextBox.Text;
             string path = saveFileDialog1.FileName;
-           // FileStream fs = new FileStream(@"C:\\users\boula\desktop\employee.txt", FileMode.Create, FileAccess.Write);
+          
             FileStream fs = new FileStream(path, FileMode.Create, FileAccess.Write);
             StreamWriter sw = new StreamWriter(fs);
 
-            // do stuff to it
             sw.Write("Bob makes $10.00 per hour\nBill makes $12.00 per hour");
 
 
-            // saveFileDialogBox
+            // saveFileDialogBox is what we were using in the design portion to open file explorer
 
 
 
